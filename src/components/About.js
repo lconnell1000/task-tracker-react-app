55a339b99e5d5828fc9d249d0a525089c4fd1c44
lconnell1000 <lconnell1000@gmail.com>
2022-05-02 17:10:41 +0930

    finished app

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+    <h4>version 1.0.0, shoutout to traversy media!</h4>
+    <Link to = "/">Go Back</Link>
+    </div>
+  )
+}
+
+export default About
